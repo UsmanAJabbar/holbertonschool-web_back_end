05d7dcdf9e12c0421705f8f862024479e67550a4
UsmanGTA <hi@usmanjabbar.com>
2021-03-16 02:31:05 +0500

    Fixed Union where input could either be an int or float

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     ----------------
     METHOD: sum_mixed_list
