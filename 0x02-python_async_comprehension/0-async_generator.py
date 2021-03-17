831f2ddc95f89c0003a508b3ecd5a6427de2b072
UsmanGTA <hi@usmanjabbar.com>
2021-03-17 16:45:42 +0500

    Added PEP8 fixes

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,15 +6,15 @@ from typing import List
 
 
 async def async_generator() -> float:
-	"""
-	-----------------------
-	METHOD: async_generator
-	-----------------------
-	Description:
-		Generates and returns a float
-		n number of times to an async
-		generator call in main()
-	"""
-	for i in range(10):
-		await asyncio.sleep(1)
-		yield random.uniform(0, 10)
+    """
+    -----------------------
+    METHOD: async_generator
+    -----------------------
+    Description:
+        Generates and returns a float
+        n number of times to an async
+        generator call in main()
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
