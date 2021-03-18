300dcf0a6ea8e46d7e50c2ec2a3922cc48e533a4
UsmanGTA <hi@usmanjabbar.com>
2021-03-18 14:37:35 +0500

    Added initial commit for task 1

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""Basic Caching"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    ----------------
+    CLASS: FIFOCache
+    ----------------
+    """
+
+    def __init__(self):
+        """
+        ----------------------
+        MAGIC METHOD: __init__
+        ----------------------
+        Description:
+                Initializes the current
+                class object
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        -----------
+        METHOD: put
+        -----------
+        Description:
+                Adds to caching dictionary an item
+                provided a key
+        Args:
+                @key: key to add to the cache
+                @item: value to add to the cache
+        """
+        if len(self.cache_data) > 4:
+            fifo_key = next(iter(self.cache_data))
+            print('DISCARD:', fifo_key)
+            del self.cache_data[fifo_key]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        -----------
+        METHOD: get
+        -----------
+        Description:
+                Given a key, returns the element
+                from cache_data if the key exists
+                in the cache_data dictionary.
+        Args:
+                @key: key to look for in cache
+        """
+        if not key or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
