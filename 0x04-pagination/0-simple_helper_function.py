f73494598ebf0ad7ce30010bd3c43a4cfd179f7a
UsmanGTA <hi@usmanjabbar.com>
2021-03-23 16:29:35 +0500

    Added initial commit for task 0

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    -------------------
+    METHOD: index_range
+    -------------------
+    Description:
+        Takes in a starting page defined by page
+        that acts as an offset and page_size
+        that acts as the LIMIT.
+    Args:
+        @page      :offset
+        @page_size :limit
+    """
+    if page <= 0:
+        page = 1
+    start = (page - 1) * page_size
+    limit = page * page_size
+
+    return (start, limit)
