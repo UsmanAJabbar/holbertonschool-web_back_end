4ff1cc37320855aa0eaca30b449888630f9c967a
UsmanGTA <hi@usmanjabbar.com>
2021-03-29 21:43:43 +0500

    Disabled functions

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -33,69 +33,69 @@ def filter_datum(fields: List[str],
     return message
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class"""
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+# class RedactingFormatter(logging.Formatter):
+#     """ Redacting Formatter class"""
+#     REDACTION = "***"
+#     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+#     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]):
-        """Initializes a redacting formatter"""
-        self.fields = list(fields)
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+#     def __init__(self, fields: List[str]):
+#         """Initializes a redacting formatter"""
+#         self.fields = list(fields)
+#         super(RedactingFormatter, self).__init__(self.FORMAT)
 
-    def format(self, record: logging.LogRecord) -> str:
-        """Format method that redacts sensitive information"""
-        return filter_datum(self.fields,
-                            self.REDACTION,
-                            super().format(record),
-                            self.SEPARATOR)
-
-
-def get_logger() -> logging.Logger:
-    """
-    ------------------
-    METHOD: get_logger
-    ------------------
-    """
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-
-    handler = logging.StreamHandler()
-    handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
-
-    logger.addHandler(handler)
-    return logger
+#     def format(self, record: logging.LogRecord) -> str:
+#         """Format method that redacts sensitive information"""
+#         return filter_datum(self.fields,
+#                             self.REDACTION,
+#                             super().format(record),
+#                             self.SEPARATOR)
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """
-    --------------
-    METHOD: get_db
-    --------------
-    Description:
-        Takes no arguments, but returns
-        a connection to the database.
-    """
-    from os import environ as env
+# def get_logger() -> logging.Logger:
+#     """
+#     ------------------
+#     METHOD: get_logger
+#     ------------------
+#     """
+#     logger = logging.getLogger("user_data")
+#     logger.setLevel(logging.INFO)
+#     logger.propagate = False
 
-    usr = os.env['PERSONAL_DATA_DB_USERNAME']
-    pwd = os.env['PERSONAL_DATA_DB_PASSWORD']
-    host = os.env['PERSONAL_DATA_DB_HOST']
-    db = os.env['PERSONAL_DATA_DB_NAME']
+#     handler = logging.StreamHandler()
+#     handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
 
-    return mysql.connector.connect(user=usr,
-                                   password=pwd,
-                                   host=host,
-                                   database=db)
+#     logger.addHandler(handler)
+#     return logger
 
 
-if __name__ == "main":
-    """Main function for some reason"""
-    db_data = get_db()
-    db_query = db_data.cursor()
-    db_query.execute('SELECT * FROM users;')
+# def get_db() -> mysql.connector.connection.MySQLConnection:
+#     """
+#     --------------
+#     METHOD: get_db
+#     --------------
+#     Description:
+#         Takes no arguments, but returns
+#         a connection to the database.
+#     """
+#     from os import environ as env
 
-    for rows in db_query:
-        print(''.join(str(rows)))
+#     usr = os.env['PERSONAL_DATA_DB_USERNAME']
+#     pwd = os.env['PERSONAL_DATA_DB_PASSWORD']
+#     host = os.env['PERSONAL_DATA_DB_HOST']
+#     db = os.env['PERSONAL_DATA_DB_NAME']
+
+#     return mysql.connector.connect(user=usr,
+#                                    password=pwd,
+#                                    host=host,
+#                                    database=db)
+
+
+# if __name__ == "main":
+#     """Main function for some reason"""
+#     db_data = get_db()
+#     db_query = db_data.cursor()
+#     db_query.execute('SELECT * FROM users;')
+
+#     for rows in db_query:
+#         print(''.join(str(rows)))
