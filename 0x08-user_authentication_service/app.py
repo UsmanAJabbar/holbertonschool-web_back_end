620a971fe091ea4c1ffed52ef941902e08890a3f
UsmanGTA <hi@usmanjabbar.com>
2021-04-09 04:35:46 +0500

    Fixed and switched user.user_id with user.id

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -78,7 +78,7 @@ def logout():
     if session_id:
         user = AUTH.get_user_from_session_id(session_id)
         if user:
-            AUTH.destroy_session(user.user_id)
+            AUTH.destroy_session(user.id)
             return redirect('/')
 
     abort(403)
