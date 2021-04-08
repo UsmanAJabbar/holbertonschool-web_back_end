4b562b18054c4995ce0a5381691918aaebb9b62b
UsmanGTA <hi@usmanjabbar.com>
2021-04-09 03:54:13 +0500

    Added get_reset_password_token

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -130,3 +130,21 @@ class Auth:
                 self._db.update_user(user.user_id, session_id=None)
             except NoResultFound:
                 pass
+
+
+    def get_reset_password_token(email: str) -> str:
+        """
+        --------------------------------
+        METHOD: get_reset_password_token
+        --------------------------------
+        Description:
+            Returns a password reset token
+        """
+        if type(email) is str:
+            try:
+                user = self._db.find_user_by(email=email)
+                reset_token = _generate_uuid()
+                self._db.update_user(user.user_id, reset_token=reset_token)
+                return reset_token
+            except NoResultFound:
+                raise ValueError
