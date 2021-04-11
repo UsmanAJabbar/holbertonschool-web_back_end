ec76ea39b15220e82cf12a6894f33d4c50ed3a7b
UsmanGTA <hi@usmanjabbar.com>
2021-04-12 02:20:42 +0500

    Added docstring to the top of the file

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Some random docstring outta nowhere"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
