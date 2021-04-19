ef603b47b0826d38dc27de32281c2b0c893d2e8a
UsmanGTA <hi@usmanjabbar.com>
2021-04-19 21:09:25 +0500

    Added

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -2,13 +2,21 @@
 """Test Client File"""
 from client import GitHubOrgClient
 from parameterized import parameterized
+from unittest.mock import patch, Mock
+import unittest
 
 
-class TestGitHubOrgClient(unittest.TestCase)
+class TestGitHubOrgClient(unittest.TestCase):
 
-	@parameterized.expand([
-		('google'),
-		('abc')
-	])
-    def test_org(self, gh_user):
-		
+    @parameterized.expand([
+        ('google'),
+        ('abc')
+    ])
+    @patch('client.get_json')
+    def test_org(self, gh_user, mocked_method):
+        mocked_method.return_value = [gh_user]
+        gh = GitHubOrgClient(gh_user)
+        self.assertIn(gh_user, gh.org)
+
+if __name__ == '__main__':
+    unittest.main()
