ec79d103eb55c8e72458d13b883b737612fd37ed
UsmanGTA <hi@usmanjabbar.com>
2021-04-22 04:05:46 +0500

    Remove unneeded imports

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Basic Flask App """
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template
 from flask_babel import Babel
 
 class Config(object):
@@ -18,7 +18,7 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 app = Flask(__name__)
-app.config.from_object('Config')
+app.config.from_object(Config)
 babel = Babel(app)
 
 @app.route('/', methods=['GET'], strict_slashes=False)
