0d5d12e803af5e04ae09de3f3c1905f6c25ce586
UsmanGTA <hi@usmanjabbar.com>
2021-04-27 04:29:14 +0500

    Added initial commit for task 2 for debug

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,6 +4,25 @@ import redis
 from typing import Union, Callable, Optional, Any
 
 
+def count_calls(fn: Callable) -> Callable:
+    """
+    -----------------------
+    METHOD: count_decorator
+    -----------------------
+    Description:
+        Decorator that counts the number of
+        times a method was called.
+    """
+    key = fn.__qualname__
+    print(key)
+    def key_setter(self, *args, **kwargs):
+        print(key, arg, kwargs)
+        if hasattr(fn, key):
+            setattr(fn, key, getattr(fn, key) + 1)
+        else:
+            setattr(fn, key, 0)
+    return key_setter
+
 class Cache:
     """ Redis Caching class """
 
@@ -12,6 +31,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Adds data to the redis database """
         if data:
@@ -21,6 +41,7 @@ class Cache:
             self._redis.set(key, data)
             return key or ''
 
+    @count_calls
     def get(self, key: str, fn: Optional[Callable] = None) -> Any:
         """ Given a key, fetches data from the redis client """
         data = self._redis.get(key)
