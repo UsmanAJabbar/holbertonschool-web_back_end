bb8f5abc425c61849b290e2b5aa0057f81431a7c
UsmanGTA <hi@usmanjabbar.com>
2021-04-27 23:10:23 +0500

    Removed optional parameters from get_page

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -7,22 +7,24 @@ redis_db = redis.Redis()
 redis_db.flushdb()
 
 
-def get_page(url: str = 'http://slowwly.robertomurray.co.uk',
-             exp: int = 10) -> None:
+def get_page() -> None:
     """ Runs a GET request on a given URL """
-    if type(url) is str and type(exp) is int:
-        req = r_get(url)
 
-        if not redis_db.get(f"count:{url}"):
-            redis_db.set(f"count:{url}", 1, exp)
-        else:
-            redis_db.incr(f"count:{url}")
+    url = 'http://slowwly.robertomurray.co.uk'
+    exp = 10
 
-        return req.text
+    req = r_get(url)
 
-# get_page('http://slowwly.robertomurray.co.uk')
-# get_page('http://slowwly.robertomurray.co.uk')
-# get_page('https://usmanjabbar.com')
-# get_page()
-# print(redis_db.get(f"count:http://slowwly.robertomurray.co.uk"))
-# print(redis_db.get(f"count:https://usmanjabbar.com"))
+    if not redis_db.get(f"count:{url}"):
+        redis_db.set(f"count:{url}", 1, exp)
+    else:
+        redis_db.incr(f"count:{url}")
+
+    return req.text
+
+get_page()
+get_page()
+get_page()
+get_page()
+print(redis_db.get(f"count:http://slowwly.robertomurray.co.uk"))
+print(redis_db.get(f"count:https://usmanjabbar.com"))
