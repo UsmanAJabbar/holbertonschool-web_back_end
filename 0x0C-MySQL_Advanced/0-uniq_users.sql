4b8f8f6e3a328bc6f80eb6a8b5f80bde953d9fef
UsmanGTA <usmangta@gmail.com>
2021-04-29 07:34:48 +0500

    Added initial commit for task 0

---
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Creates a table called users with
+-- edge cases to ensure it never fails
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `email` varchar(255) NOT NULL UNIQUE,
+    `name` varchar(255)
+);
