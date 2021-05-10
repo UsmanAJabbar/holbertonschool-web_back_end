bc60da45b1b13f01b106bc130276db28099dcc92
UsmanGTA <usmangta@gmail.com>
2021-05-11 00:45:30 +0500

    Added initial commit for task 9

---
+++ 0x0F-ES6_promise/9-try.js
@@ -0,0 +1,8 @@
+// Some salty doc
+export default function guardrail(mathFunction) {
+  try {
+    return [mathFunction(), 'Guardrail was processed'];
+  } catch (e) {
+    return [e.toString(), 'Guardrail was processed'];
+  }
+}
