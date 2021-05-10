eee1eaac050e64474e9360f19c579565e7c5536b
UsmanGTA <usmangta@gmail.com>
2021-05-11 00:10:38 +0500

    Added initial commit for task 6

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+// Some random doc
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ]).then(() => {
+  }).catch(() => {
+  }).finally((values) => values);
+}
