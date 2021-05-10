67b8412d1bc4c975baf0f1447bc773ab355fed0c
UsmanGTA <usmangta@gmail.com>
2021-05-11 00:34:48 +0500

    Added initial commit for task 8

---
+++ 0x0F-ES6_promise/8-try.js
@@ -0,0 +1,8 @@
+// Some doc
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  } else {
+    return numerator / denominator;
+  }
+}
