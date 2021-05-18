32a14f9627ea20da65a4893ce3334b8b62782928
UsmanGTA <usmangta@gmail.com>
2021-05-18 19:45:41 +0500

    Added fixes

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,13 @@
 // Given a set of strings, removes startString from every string in the set
 export default function cleanSet(set, startString) {
-  if (!(set instanceof Set) || startString === '') return '';
+  if (startString === '') return '';
 
-  return [...set]
-    .filter((str) => str.startsWith(startString))
-    .map((str) => str.replace(startString, ''))
-    .join('-');
+  try {
+    return [...set]
+      .filter((str) => str.startsWith(startString))
+      .map((str) => str.replace(startString, ''))
+      .join('-');
+  } catch (e) {
+    return '';
+  }
 }
