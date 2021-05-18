39aaf0cd390b1bd9492b7c2fb0139184665ddd05
UsmanGTA <usmangta@gmail.com>
2021-05-18 16:53:41 +0500

    Added ESlint fixes

--- 0x11-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 // Returns the ids found in an arrayOfObjects
 export default function getListStudentIds(arrayOfObjects) {
-  let arr = [];
+  const arr = [];
   if (Array.isArray(arrayOfObjects)) {
     return arrayOfObjects.map((item) => item.id);
   }
