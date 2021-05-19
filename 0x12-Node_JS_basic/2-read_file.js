0c4feceda7a44dca870ee070bd20e1f0c70dc304
UsmanGTA <usmangta@gmail.com>
2021-05-19 23:19:24 +0500

    Added initial commit for task 2

---
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+  try {
+    // Pull data from CSV file
+    const fileData = fs.readFileSync(path, 'utf8')
+      .split('\n')
+      .map((student) => student.split(','))
+      .slice(1)
+      .reduce((total, currentVal) => {
+        const eslint = total;
+        const subj = currentVal[currentVal.length - 1];
+        const firstName = currentVal[0];
+
+        if (typeof eslint[subj] === 'undefined') eslint[subj] = [firstName];
+        else eslint[subj].push(firstName);
+
+        return eslint;
+      }, {});
+
+    // Prepare Print Statements
+    let totalStudents = 0;
+    let output = '';
+
+    for (const field in fileData) {
+      if (fileData.hasOwnProperty) {
+        totalStudents += fileData[field].length;
+        output += `Number of students in ${field}: ${fileData[field].length}. List: ${fileData[field].join(', ')}\n`;
+      }
+    }
+
+    // Print necessary print statements
+    console.log(`Number of students: ${totalStudents}`);
+    console.log(output.slice(0, -1));
+  } catch (e) {
+    throw Error('Cannot load the database');
+  }
+};
+module.exports = countStudents;
