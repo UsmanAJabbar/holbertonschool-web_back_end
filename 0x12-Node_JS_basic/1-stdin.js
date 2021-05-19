b895f5c956ad8889cb34135466cc597361a1fcc9
UsmanGTA <usmangta@gmail.com>
2021-05-19 18:00:19 +0500

    Added edge case fixes

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin
-  .on('data', (input) => {
-    process.stdout.write(`Your name is: ${input}`);
-    process.exit();
+  .on('readable', () => {
+    const input = process.stdin.read();
+    if (input) process.stdout.write(`Your name is: ${input}`);
   })
   .on('end', () => {
     process.stdout.write('This important software is now closing\n');
