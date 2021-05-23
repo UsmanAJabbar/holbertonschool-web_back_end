86fea39f373141e9b121680384f7ea1cd631d75b
UsmanGTA <usmangta@gmail.com>
2021-05-23 23:26:49 +0500

    Added fixes

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -2,41 +2,45 @@ const fs = require('fs');
 
 const countStudents = (path) => new Promise((resolve, reject) => {
   // Pull data from CSV file
-  fs.readFile(path, 'utf-8', (err, data) => {
-    if (err) reject(Error('Cannot load the database'));
-    else {
-      const fileData = data
-        .split('\n')
-        .map((student) => student.split(','))
-        .slice(1)
-        .reduce((total, currentVal) => {
-          const eslint = total;
-          const subj = currentVal[currentVal.length - 1];
-          const firstName = currentVal[0];
+  try {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) reject(Error('Cannot load the database'));
+      else {
+        const fileData = data
+          .split('\n')
+          .map((student) => student.split(','))
+          .slice(1)
+          .reduce((total, currentVal) => {
+            const eslint = total;
+            const subj = currentVal[currentVal.length - 1];
+            const firstName = currentVal[0];
 
-          if (typeof eslint[subj] === 'undefined') eslint[subj] = [firstName];
-          else eslint[subj].push(firstName);
+            if (typeof eslint[subj] === 'undefined') eslint[subj] = [firstName];
+            else eslint[subj].push(firstName);
 
-          return eslint;
-        }, {});
+            return eslint;
+          }, {});
 
-      // Prepare Print Statements
-      let totalStudents = 0;
-      let output = '';
+        // Prepare Print Statements
+        let totalStudents = 0;
+        let output = '';
 
-      for (const field in fileData) {
-        if (fileData.hasOwnProperty) {
-          totalStudents += fileData[field].length;
-          output += `Number of students in ${field}: ${fileData[field].length}. List: ${fileData[field].join(', ')}\n`;
+        for (const field in fileData) {
+          if (fileData.hasOwnProperty) {
+            totalStudents += fileData[field].length;
+            output += `Number of students in ${field}: ${fileData[field].length}. List: ${fileData[field].join(', ')}\n`;
+          }
         }
-      }
 
-      // Print necessary print statements
-      output = (`Number of students: ${totalStudents}\n${output}`).slice(0, -1);
-      console.log(output);
-      resolve(output);
-    }
-  });
+        // Print necessary print statements
+        output = (`Number of students: ${totalStudents}\n${output}`).slice(0, -1);
+        console.log(output);
+        resolve(output);
+      }
+    });
+  } catch (e) {
+    reject(Error('Cannot load the database'));
+  }
 });
 
 module.exports = countStudents;
