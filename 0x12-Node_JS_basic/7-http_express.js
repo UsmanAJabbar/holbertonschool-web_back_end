9fb4084c4aea69c548e396e609d219b77913a001
UsmanGTA <usmangta@gmail.com>
2021-05-23 23:09:52 +0500

    Added initial commit for task 7

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -7,12 +7,15 @@ const countStudents = require('./3-read_file_async');
 const pathToCSVFile = process.argv[2];
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+  res.status(200).send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
   countStudents(pathToCSVFile)
-    .then((success) => res.send(success));
+    .then((success) => {
+      const out = `This is the list of our students\n${success}`;
+      res.status(200).send(out);
+    });
 });
 
 module.exports = app;
