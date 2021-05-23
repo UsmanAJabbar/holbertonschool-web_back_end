641df2d8a4c1640e308761ed58a3d29b058054df
UsmanGTA <usmangta@gmail.com>
2021-05-23 23:57:41 +0500

    Added fixes

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -1,17 +1,16 @@
 const app = require('express')();
-
-app.listen(1245);
-
 const countStudents = require('./3-read_file_async');
-
 const pathToCSVFile = process.argv[2];
+
+// Setup a listener
+app.listen(1245);
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  countStudents(pathToCSVFile)
+  countStudents(pathToCSVFile, 0)
     .then((success) => {
       const out = `This is the list of our students\n${success}`;
       res.status(200).send(out);
