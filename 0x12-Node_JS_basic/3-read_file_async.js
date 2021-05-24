39353da2f0b05373cb2ed24520d742fbd094afad
UsmanGTA <usmangta@gmail.com>
2021-05-24 23:22:27 +0500

    Added supposed fixes

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -27,10 +27,8 @@ const countStudents = (path, printFlag = 1) => new Promise((resolve, reject) => 
         let output = '';
 
         for (const field in fileData) {
-          if (fileData.hasOwnProperty) {
-            totalStudents += fileData[field].length;
-            output += `Number of students in ${field}: ${fileData[field].length}. List: ${fileData[field].join(', ')}\n`;
-          }
+          totalStudents += fileData[field].length;
+          output += `Number of students in ${field}: ${fileData[field].length}. List: ${fileData[field].join(', ')}\n`;
         }
 
         // Print necessary print statements
