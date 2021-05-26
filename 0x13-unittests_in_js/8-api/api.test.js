2d52643a87b5625d04c12c03346a334e0e506d5e
UsmanGTA <usmangta@gmail.com>
2021-05-27 01:08:48 +0500

    Added fixes

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -3,8 +3,15 @@ const request = require('request');
 
 describe('index page test', () => {
 
-  it('Returns the right content', function (done) {
+  it('Tests non / endpoint', function (done) {
     request('http://localhost:7865', function (error, response, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+  it('Tests / endpoint', function (done) {
+    request('http://localhost:7865/', function (error, response, body) {
       expect(body).to.equal('Welcome to the payment system');
       expect(response.statusCode).to.equal(200);
       done();
