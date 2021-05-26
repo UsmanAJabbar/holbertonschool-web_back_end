b1bac859279b868cd0739965652db2764feb316b
UsmanGTA <usmangta@gmail.com>
2021-05-27 01:07:07 +0500

    Added fixes

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -1,28 +1,14 @@
 const { expect } = require('chai');
-const ping = require('./request');
+const request = require('request');
 
 describe('index page test', () => {
-  it('test API root endpoint with /', async () => {
-    await new Promise((done) => {
-      ping('http://localhost:7865')
-        .then((data) => expect(data === 'Welcome to the payment system').to.be.true);
+
+  it('Returns the right content', function (done) {
+    request('http://localhost:7865', function (error, response, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
-  it('test API root endpoint w/o /', async () => {
-    await new Promise((done) => {
-      ping('http://localhost:7865')
-        .then((data) => expect(data === 'Welcome to the payment system').to.be.true);
-      done();
-    });
-  });
-
-  it('test unknown endpoint', async () => {
-    await new Promise((done) => {
-      ping('http://localhost:7865/end')
-        .catch((err) => expect(err).contains('Cannot GET /end'));
-      done();
-    });
-  });
 });
