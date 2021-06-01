07cd67c486065100d3ba90e4f5df48d52800afb4
UsmanGTA <usmangta@gmail.com>
2021-06-01 15:37:36 +0500

    Simplified task 6

--- 0x14-queuing_system_in_js/6-job_creator.js
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -2,14 +2,14 @@ const kue = require('kue'),
       queue = kue.createQueue();
 
 const jobData = {
-  phoneNumber: 'string',
-  message: 'string',
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
 }
 
 let job = queue
   .create('push_notification_code', jobData)
   .save((err) => {
     if (!err) console.log(`Notification job created: ${job.id}`)
-    else console.log('Notification job failed');
   })
-  .on('complete', () => console.log('Notification job failed'));
+  .on('complete', () => console.log('Notification job completed'))
+  .on('failed', () => console.log('Notification job failed'));
